e2e02dc7ef3ee1742acfec5fc47d49f9d76f8edb
Naresh Bhatia <me@nareshbhatia.dev>
2023-01-08 13:59:55 -0500

    Replaced atomic colors in Button and Hero with semantic colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,9 @@ import clsx from 'clsx';
 
 const variantStyles: { [variant: string]: string } = {
   primary:
-    'inline-flex items-center rounded-md border border-transparent bg-primary-600 px-8 py-2 text-lg font-semibold text-white shadow-sm hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2',
+    'inline-flex items-center rounded-md border border-transparent px-8 py-2 text-lg font-semibold bg-brand text-onbrand hover:bg-brand-hover focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 shadow-sm',
   secondary:
-    'inline-flex items-center rounded-md border border-transparent bg-primary-100 px-8 py-2 text-lg font-semibold text-primary-700 hover:bg-primary-200 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2',
+    'inline-flex items-center rounded-md border border-transparent px-8 py-2 text-lg font-semibold bg-brand-secondary text-onbrand-secondary hover:bg-brand-secondary-hover focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2',
 };
 
 // @ts-ignore
