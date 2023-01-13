7de6bd94a28a010a6924711c1b9c1d8a5f5bfe81
Naresh Bhatia <me@nareshbhatia.dev>
2023-01-12 22:45:10 -0500

    Fixed issue with ModeToggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: ['class', 'theme-dark'],
+  darkMode: ['class', '.theme-dark'],
   theme: {
     colors: {},
     backgroundColor: {
