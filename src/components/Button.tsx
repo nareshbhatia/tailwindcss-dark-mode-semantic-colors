9e8d2bd850853cabb68f1ef5e2dfae32eae00e36
Naresh Bhatia <me@nareshbhatia.dev>
2023-01-14 11:33:54 -0500

    Refactored button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,15 @@ import clsx from 'clsx';
 
 const variantStyles: { [variant: string]: string } = {
   primary:
-    'inline-flex items-center rounded-md border border-transparent px-8 py-2 text-lg font-semibold bg-brand text-onbrand hover:bg-brand-hover focus:outline-none focus:ring-2 focus:ring-brand focus:ring-offset-2 shadow-sm',
+    'bg-brand text-onbrand hover:bg-brand-hover focus:outline-none focus:ring-2 focus:ring-brand focus:ring-offset-2 shadow-sm',
   secondary:
-    'inline-flex items-center rounded-md border border-transparent px-8 py-2 text-lg font-semibold bg-secondary text-onsecondary hover:bg-secondary-hover focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2',
+    'bg-secondary text-onsecondary hover:bg-secondary-hover focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2',
 };
 
 // @ts-ignore
 export function Button({ variant = 'primary', className, children, ...props }) {
   className = clsx(
-    'inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition',
+    'inline-flex items-center justify-center overflow-hidden rounded-md border border-transparent px-8 py-2 text-sm font-medium',
     variantStyles[variant],
     className
   );
